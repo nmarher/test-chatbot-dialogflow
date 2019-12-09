bdcf700a97232c518bb7932c8ce526a57e7bf677
Noe <noelia.martin.hernandez@gmail.com>
2019-12-09 10:29:28 +0100

    A

--- chat-bot.js
+++ chat-bot.js
@@ -68,7 +68,7 @@ $(function () {
 
        var u = new SpeechSynthesisUtterance();
        u.text = message;
-       u.lang = 'en-US';
+       u.lang = 'es-ES';
        u.rate = 1;
        u.onend = function(event) {
             $('#mic').click();
